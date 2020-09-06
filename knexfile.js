9e9be2369c60505fcc83c927190bbdec6dca644d
Mahmoud Masters <mahmoudaburascea@gmail.com>
2020-09-06 10:35:56 +0200

    FEATURE: knexfile.js configuration file for knex to configure CLI

---
+++ knexfile.js
@@ -0,0 +1,59 @@
+// Update with your config settings.
+
+module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+  development: {
+    client: 'mysql2',
+    connection: {
+      database: 'ip_uptime_scanner',
+      user:     'root',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+  },
+
+  staging: {
+    client: 'mysql2',
+    connection: {
+      database: 'ip_uptime_scanner',
+      user:     'root',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      database: 'ip_uptime_scanner',
+      user:     'root',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
