19db51320209314883376f6fe8c129b52525bae0
Mahmoud Masters <mahmoudaburascea@gmail.com>
2020-09-07 14:39:57 +0200

    FEATURE: Added delete and put routes and cleaned up the code

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -3,41 +3,56 @@ var router = express.Router();
 
 var knex = require('../services/knexConnection');
 
+
 router.get('/', (req, res) => {
+
     knex('clients')
-        .then(rows => {
-            console.log(rows);
-            res.status(200).json(rows);
-        });
+        .then(rows => { res.status(200).json(rows); });
+
 });
 
 router.get('/:id', (req, res) => {
-    console.log(req.params);
+
     knex('clients').where('id', req.params.id)
-        .then(rows => {
-            console.log(rows);
-            res.status(200).json(rows);
-        })
+        .then(rows => { res.status(200).json(rows); })
+
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
 
     client_to_insert = {
         name: req.body.name,
         address: req.body.address,
-        status: req.body.status,
         type: req.body.type,
         attributes: JSON.stringify(req.body.attributes),
         description: req.body.description,
     };
-    
-    console.log(req.body, client_to_insert);
 
     knex('clients').insert(client_to_insert)
-        .then(data => {
-            res.status(200).json(data);
-        });
+        .then(data => { res.status(200).json(data); });
+
+});
+
+router.put('/:id', (req, res) => {
+
+    client_to_update = {
+        name: req.body.name,
+        address: req.body.address,
+        type: req.body.type,
+        attributes: JSON.stringify(req.body.attributes),
+        description: req.body.description,
+    };
+
+    knex('clients').where('id', req.params.id).update(client_to_update)
+        .then(data => { res.status(200).json(data); });
+
+});
+
+router.delete('/:id', (req, res) => {
+
+    knex('clients').where('id', req.params.id).del()
+        .then(data => { res.status(200).json(data); })
+
 });
 
 module.exports = router;
