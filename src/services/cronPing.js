a997faf92fe39a13008f444e9d5f93816d3a29c0
Mahmoud Masters <mahmoudaburascea@gmail.com>
2020-09-07 14:40:54 +0200

    FEATURE: Now checks for change in status, updates it and date, then adds to history if a change has occured, also activates once a minute

--- src/services/cronPing.js
+++ src/services/cronPing.js
@@ -2,28 +2,55 @@ const knex = require('./knexConnection');
 const cron = require('node-cron');
 const http = require('http');
 
-cron.schedule('*/15 * * * * *', () => {
+cron.schedule('* * * * *', () => {
     console.log("This is a cron job running on the server.");
     knex('clients')
         .then(rows => {
-            console.log("Getting clients: " + JSON.stringify(rows.length));
+            console.log("Getting clients: " + rows.length);
             rows.forEach(client => {
-                console.log("client: " + JSON.stringify(client));
+                console.log("Pinging client: " + client.address);
 
                 options = {
                     host: client.address,
                 }
 
+                // check if the client is up or down and set their status if they've changed
                 var req = http.get(options, res => {
-                    console.log("Ping " + client.address + " Success.");
-                    knex('clients').where({id: client.id}).update({status: 1})
-                        .then(res => {});
+                    if(client.status != 'up'){
+                        console.log("Client " + client.address + " Is back up!");
+                        knex('clients').where({id: client.id}).update({status: 'up', updated_at: knex.fn.now()})
+                            .then(res => {
+                                // save to history log
+                                client_history = {
+                                    'id': client.id,
+                                    'address': client.address,
+                                    'status': client.address
+                                }
+                                knex('clients_hist').insert(client_history)
+                                    .then(res => {})
+                            });
+                        } else {
+                            console.log("Client " + client.address + " Is still up.");
+                        }
                 })
 
                 req.on('error', error => {
-                    console.log("Ping " + client.address + " error: "+ JSON.stringify(error));
-                    knex('clients').where({id: client.id}).update({status: 0})
-                        .then(res => {});
+                    if(client.status != 'down'){
+                        console.log("Client " + client.address + " Is down!: "+ JSON.stringify(error));
+                        knex('clients').where({id: client.id}).update({status: 'down', updated_at: knex.fn.now()})
+                            .then(res => {
+                                // save to history log
+                                client_history = {
+                                    'id': client.id,
+                                    'address': client.address,
+                                    'status': client.address
+                                }
+                                knex('clients_hist').insert(client_history)
+                                    .then(res => {})
+                            });
+                        } else {
+                            console.log("Client " + client.address + " Is still down. "+ JSON.stringify(error));
+                        }
                 })
             })
         })
